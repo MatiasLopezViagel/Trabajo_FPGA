eee0ad9020a8a54dcb0df97802a211ec5806fa5a
MatiasLopezViagel <100703037+MatiasLopezViagel@users.noreply.github.com>
2025-12-05 14:46:54 +0100

    Creación de la carpeta del proyecto

---
+++ Trabajo_FPGA/Trabajo_FPGA.srcs/sources_1/new/Source.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05.12.2025 14:38:50
+-- Design Name: 
+-- Module Name: Source - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Source is
+--  Port ( );
+end Source;
+
+architecture Behavioral of Source is
+
+begin
+
+
+end Behavioral;
